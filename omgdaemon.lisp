16d939da1990a359e0e174d93ec182d9ea1f85bc
hemml <hemml@me.com>
2022-04-07 14:20:07 +0300

    Disable debugger in non-interactive image build

--- omgdaemon.lisp
+++ omgdaemon.lisp
@@ -528,10 +528,10 @@
             "RUN apt-get update && apt-get dist-upgrade -y"
             "RUN apt-get install -y sbcl cl-quicklisp git build-essential"
             "RUN adduser omg"
-            "RUN su -l omg -c 'sbcl --load \"/usr/share/common-lisp/source/quicklisp/quicklisp.lisp\" --eval \"(quicklisp-quickstart:install)\"'"
+            "RUN su -l omg -c 'sbcl --load \"/usr/share/common-lisp/source/quicklisp/quicklisp.lisp\" --eval \"(quicklisp-quickstart:install)\" --disable-debugger'"
             "RUN su -l omg -c 'mkdir -p /home/omg/quicklisp/local-projects && cd /home/omg/quicklisp/local-projects && git clone --recurse-submodules https://github.com/hemml/OMGlib.git'"
-            "RUN su -l omg -c 'cd /home/omg && sbcl --eval \"(load \\\"/home/omg/quicklisp/setup.lisp\\\")\" --eval \"(ql:quickload :omg)\" --eval \"(omgdaemon:make-omg-daemon 8080)\"'"
-            "EXPOSE 80 4008"
+            "RUN su -l omg -c 'cd /home/omg && sbcl --eval \"(load \\\"/home/omg/quicklisp/setup.lisp\\\")\" --eval \"(ql:quickload :omg)\" --eval \"(omgdaemon:make-omg-daemon 8080)\" --disable-debugger'"
+            "EXPOSE 8080 4008"
             "CMD while true; do su -l omg -c 'cd /home/omg && ./omgdaemon' ; sleep 1 ; done")))
     (run '(docker build :tag omgdaemon :pull :no-cache -) :input fd)))
 
